46af3440763059fc7eb0c8965abab96494a24c54
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2021-10-19 23:20:26 -0400

    xls reading trials

---
+++ src/test/java/com/cybertek/translation/ReadXLSX.java
@@ -0,0 +1,35 @@
+package com.cybertek.translation;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class ReadXLSX {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fisReading = new FileInputStream("D:\\xlsx\\source.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook (fisReading);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+
+        //reading from cells using Iterator:
+        Iterator<Row> ite = sheet.rowIterator(); // ite: rows
+        while(ite.hasNext()){
+            Row row = ite.next();
+            Iterator<Cell> cite = row.cellIterator();
+            while(cite.hasNext()){
+                XSSFCell c = (XSSFCell) cite.next();
+                System.out.print(c.toString() +"  ");
+            }
+            System.out.println();// ending of a row
+        } //ending of all rows
+
+        fisReading.close();
+
+    }
+}
