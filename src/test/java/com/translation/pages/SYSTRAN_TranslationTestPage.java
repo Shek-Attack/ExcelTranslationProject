89292fcc3b609395ebc9a09d954cd9c35620421a
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2021-11-24 00:16:24 -0500

    xls read, translate, write success with Google Translator: row and column numbers are taken from the file automatically.

---
+++ src/test/java/com/translation/pages/SYSTRAN_TranslationTestPage.java
@@ -0,0 +1,29 @@
+package com.translation.pages;
+
+import com.translation.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SYSTRAN_TranslationTestPage {
+
+    public SYSTRAN_TranslationTestPage(){PageFactory.initElements(Driver.getDriver(), this);}
+
+    //@FindBy(xpath = "(//textarea)[1]") // DeepL
+    //@FindBy(xpath = " //textarea") // Google Translate
+    @FindBy(xpath = "//textarea") // SYSTRAN text-area
+    public WebElement sourceTextArea;
+
+    //@FindBy(xpath = "(//textarea)[2]") // DeepL
+    @FindBy(xpath = "//div[@id='translateContent']") // SYSTRAN translated text-area
+    //@FindBy(xpath = " //pre[@id='tw-target-text']") // Google Translate
+    //@FindBy(xpath = "//span[@lang='ug']") // Google Translate UYGHUR
+    public WebElement transTextArea;
+
+    public String getTranslation(){
+        return transTextArea.getText();
+    }
+
+
+
+}
